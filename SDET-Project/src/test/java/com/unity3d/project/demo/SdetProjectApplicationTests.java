d3e1f851a439d96cb3de1bc1e595b30389f6bad1
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-05-21 14:24:41 -0700

    Refactor and setUp '/requestProject' API tests

--- SDET-Project/src/test/java/com/unity3d/project/demo/SdetProjectApplicationTests.java
+++ SDET-Project/src/test/java/com/unity3d/project/demo/SdetProjectApplicationTests.java
@@ -2,8 +2,11 @@ package com.unity3d.project.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,6 +16,9 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.unity3d.project.controller.ProjectController;
 import com.unity3d.project.model.KeysWrapper;
 import com.unity3d.project.model.Project;
 
@@ -23,7 +29,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.json.Json;
@@ -41,173 +51,208 @@ public class SdetProjectApplicationTests {
 
 	private MockMvc mockMvc;
 
+	/** Logger **/
+	private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
+
+	/** Default error response in json string */
+	private final static String jsonErrorResponse = Json.createObjectBuilder().add("message", "no project found")
+			.build().toString();
+
 	@Before
-	public void createMockMVC() {
+	public void setUp() {
 		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
 				./* addFilter(new GlobalFilter()) */
 				build();
+
+		// Clear and store sample projects in the "Projects.txt" every time the server is restarted
+		initializeProjects();
+
 	}
 
 	/**
-	 * Test to check whether the project is created successfully and whether it returns a correct message or not
+	 * Initializes and clears(if file contains any records) "Projects.txt" file
+	 * with sample records
+	 */
+	private void initializeProjects() {
+
+		// Create a list of sample projects
+		List<Project> projects = createDummyProjects();
+
+		// File to write/read project records
+		FileWriter file = null;
+
+		try {
+			file = new FileWriter("Projects.txt");
+			// empty the current content
+			file.write("");
+
+			// Save the created projects in "Projects.txt"
+			for (Project p : projects) {
+				// Convert the POJO into a json string format
+				Gson gson = new GsonBuilder().create();
+				String projectJson = gson.toJson(p);
+				file.write(projectJson);
+
+				// insert a line separator after each project insert
+				String newline = System.getProperty("line.separator");
+				file.append(newline);
+
+				// Log the project details
+				logger.info("Wrote record = " + p);
+			}
+		} catch (IOException e) {
+			logger.debug("Error reading file '" + file + "'");
+		} finally {
+			if (file != null) {
+				try {
+					file.close();
+				} catch (IOException ex) {
+					logger.error("Unable to write records in " + file + " file.... Due to exception = "
+							+ ex.getLocalizedMessage());
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+
+	/**
+	 * Test to check whether the project is created successfully and whether it
+	 * returns a correct message or not
+	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void testCreateProject() throws Exception {
-		// String[] countries = new String[]{"USA", "INDIA"};
 
-		List<Project> projects = createDummyProjects();
-
-		// Project project = new Project(233, "ASDASD", true, "01012010
-		// 00:00:00", "04042040 00:00:00", countriesList, 123.213,
-		// "www.sample.com", keysList);
+		Project project = new Project(7, "test project number 7", true, "12112010 00:12:22", "11082020 22:21:11",
+				Arrays.asList("IRELAND", "INDIA", "USA"), 22.23, "http://www.test.com",
+				Arrays.asList(new KeysWrapper(25, "movie"), new KeysWrapper(35, "games")));
 
 		this.mockMvc
 				.perform(post("/createproject").contentType(MediaType.APPLICATION_JSON)
-						.content(objectMapper.writeValueAsString(projects.get(0))))
+						.content(objectMapper.writeValueAsString(project)))
 				.andDo(print()).andExpect(status().is2xxSuccessful())
 				.andExpect(content().string("campaign is successfully created"));
 	}
 
 	/**
 	 * Test to check whether '/requestproject' API works accordingly
+	 * 
 	 * @throws Exception
 	 */
 	@Test
 	public void testRequestProject_AllParameters() throws Exception {
 
-		String json = Json.createObjectBuilder()
-	            .add("projectName", "test project number 1")
-	            .add("projectCost", 5.5)
-	            .add("projectUrl", "http://www.unity3d.com")
-	            .build()
-	            .toString();
+		// Project with Id=1 is expired
 		this.mockMvc
-				.perform(get("/requestproject?projectid=1&country=usa&number=29&keyword=sports").accept(MediaType.ALL))
-				.andDo(print())
-				.andExpect(status().is2xxSuccessful())
-				.andDo(print())		
-				.andExpect(content().string(json)).andDo(print());
-//				.andReturn()
+				.perform(get("/requestproject?projectid=1&country=usa&number=29&keyword=sports")
+						.accept(MediaType.APPLICATION_JSON_UTF8))
+				.andDo(print()).andExpect(status().is4xxClientError()).andDo(print())
+				.andExpect(content().string(jsonErrorResponse)).andDo(print());
 
-//		String content = result.getResponse().getContentAsString();
-//		System.out.println(content);
 	}
 
+	/**
+	 * Test to get the project details by ID
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testRequestProject_OnlyId() throws Exception {
 
-		String json = Json.createObjectBuilder()
-	            .add("projectName", "test project number 1")
-	            .add("projectCost", 5.5)
-	            .add("projectUrl", "http://www.unity3d.com")
-	            .build()
-	            .toString();
-		this.mockMvc
-				.perform(get("/requestproject?projectid=1").accept(MediaType.ALL))
-				.andDo(print())
-				.andExpect(status().is2xxSuccessful())
-				.andDo(print())		
-				.andExpect(content().string(json)).andDo(print());
+		String jsonResponse = Json.createObjectBuilder().add("projectName", "game project 4").add("projectCost", 15.5)
+				.add("projectUrl", "http://www.unity3d.com").build().toString();
+
+		this.mockMvc.perform(get("/requestproject?projectid=4").accept(MediaType.APPLICATION_JSON_UTF8)).andDo(print())
+				.andExpect(status().is2xxSuccessful()).andDo(print()).andExpect(content().string(jsonResponse))
+				.andDo(print());
 	}
 
-
+	/**
+	 * Test to get the project details by providing only target country name
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testRequestProject_OnlyCountry() throws Exception {
 
-		String json = Json.createObjectBuilder()
-	            .add("projectName", "test project number 6")
-	            .add("projectCost", 0.5)
-	            .add("projectUrl", "http://www.unity3d.com")
-	            .build()
-	            .toString();
-		this.mockMvc
-				.perform(get("/requestproject?country=usa").accept(MediaType.ALL))
-				.andDo(print())
-				.andExpect(status().is2xxSuccessful())
-				.andDo(print())		
-				.andExpect(content().string(json)).andDo(print());
+		String jsonResponse = Json.createObjectBuilder().add("projectName", "game project 4").add("projectCost", 15.5)
+				.add("projectUrl", "http://www.unity3d.com").build().toString();
+		this.mockMvc.perform(get("/requestproject?country=ireland").accept(MediaType.APPLICATION_JSON_UTF8))
+				.andDo(print()).andExpect(status().is2xxSuccessful()).andDo(print())
+				.andExpect(content().string(jsonResponse)).andDo(print());
+
 	}
 
+	/**
+	 * Test to get the project details by targetKeys--name AND
+	 * targetCountries--name
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testRequestProject_Country_Number() throws Exception {
 
-		String json = Json.createObjectBuilder()
-	            .add("projectName", "test project number 1")
-	            .add("projectCost", 0.5)
-	            .add("projectUrl", "http://www.unity3d.com")
-	            .build()
-	            .toString();
-		this.mockMvc
-				.perform(get("/requestproject?country=usa&number=20").accept(MediaType.ALL))
-				.andDo(print())
-				.andExpect(status().is2xxSuccessful())
-				.andDo(print())		
-				.andExpect(content().string(json)).andDo(print());
+		// All the projects having "usa" in their target countries are expired
+		this.mockMvc.perform(get("/requestproject?country=usa&number=20").accept(MediaType.ALL)).andDo(print())
+				.andDo(print()).andExpect(status().is4xxClientError()).andDo(print())
+				.andExpect(content().string(jsonErrorResponse)).andDo(print());
 	}
 
-			
-			
+	/**
+	 * Test to get the highest cost project when no URL parameters are provided
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void testRequestProject_NoParam() throws Exception {
 
-		String json = Json.createObjectBuilder()
-	            .add("projectName", "test project number 3")
-	            .add("projectCost", 222.5)
-	            .add("projectUrl", "http://www.unity3d.com")
-	            .build()
-	            .toString();
-		this.mockMvc
-				.perform(get("/requestproject").accept(MediaType.ALL))
-				.andDo(print())
-				.andExpect(status().is2xxSuccessful())
-				.andDo(print())		
-				.andExpect(content().string(json)).andDo(print());
+		String jsonResponse = Json.createObjectBuilder().add("projectName", "game project 4").add("projectCost", 15.5)
+				.add("projectUrl", "http://www.unity3d.com").build().toString();
+		this.mockMvc.perform(get("/requestproject").accept(MediaType.APPLICATION_JSON_UTF8)).andDo(print())
+				.andExpect(status().is2xxSuccessful()).andDo(print()).andExpect(content().string(jsonResponse))
+				.andDo(print());
 	}
-	
+
+	/**
+	 * Dummy test for hello world
+	 * @throws Exception
+	 */
 	@Test
 	public void testSayHelloWorld() throws Exception {
 		this.mockMvc.perform(get("/hello").accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
 				.andDo(print()).andExpect(status().isOk())
-				.andExpect(content().contentType("application/json;charset=UTF-8")).andDo(print());		
+				.andExpect(content().contentType("application/json;charset=UTF-8")).andDo(print());
 	}
 
 	private List<Project> createDummyProjects() {
+
+		// Project project = initializeProject(id, name, enabled, creationDate,
+		// expDate, )
+
+		Project p1 = new Project(1, "test project number 1", true, "05112017 00:00:00", "05212017 00:00:00",
+				Arrays.asList("USA", "CANADA", "MEXICO", "BRAZIL"), 5.5, "http://www.unity3d.com",
+				Arrays.asList(new KeysWrapper(25, "movie"), new KeysWrapper(30, "sports")));
+
+		Project p2 = new Project(2, "test project number 2", true, "05112017 00:00:00", "05202017 00:00:00",
+				Arrays.asList("USA", "CANADA"), 2.5, "http://www.google.com",
+				Arrays.asList(new KeysWrapper(25, "movie"), new KeysWrapper(30, "sports")));
+
+		Project p3 = new Project(3, "test project number 3", true, "03132017 00:00:00", "02202019 00:00:00",
+				Arrays.asList("CANADA", "ENGLAND", "INDIA"), 0.5, "http://www.unity3d.com",
+				Arrays.asList(new KeysWrapper(20, "cars")));
+
+		Project p4 = new Project(4, "game project 4", true, "01292017 00:00:00", "09222017 00:00:00",
+				Arrays.asList("IRELAND", "FINLAND"), 15.5, "http://www.unity3d.com",
+				Arrays.asList(new KeysWrapper(11, "games"), new KeysWrapper(10, "mobile")));
+
 		List<Project> projects = new ArrayList<Project>();
-
-		String country1 = "USA";
-		String country2 = "INDIA";
-		String country3 = "IRELAND";
-
-		List<String> countriesList = new ArrayList<String>();
-		countriesList.add(country1);
-		countriesList.add(country2);
-		countriesList.add(country3);
-
-		KeysWrapper keys = new KeysWrapper();
-		keys.setKeyword("sports");
-		keys.setNumber(20);
-
-		KeysWrapper keys2 = new KeysWrapper();
-		keys2.setKeyword("movie");
-		keys2.setNumber(25);
-
-		KeysWrapper keys3 = new KeysWrapper();
-		keys2.setKeyword("games");
-		keys2.setNumber(35);
-
-		List<KeysWrapper> keysList = new ArrayList<KeysWrapper>();
-		keysList.add(keys);
-		keysList.add(keys2);
-		keysList.add(keys3);
-
-		Double projectCost = 22.23;
-
-		Project p1 = new Project(7, "test project 7", true, "12112010 00:12:22", "11082020 22:21:11", countriesList,
-				projectCost, "www.test.com", keysList);
-
 		projects.add(p1);
+		projects.add(p2);
+		projects.add(p3);
+		projects.add(p4);
 
 		return projects;
 	}
+
 }
