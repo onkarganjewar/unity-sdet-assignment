aa06406a5545e6019d3dc1de49888c681f9fda7b
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-05-21 21:52:59 -0700

    Turn off spring boot basic security

--- SDET-Project/src/main/java/com/unity3d/project/demo/SdetProjectApplication.java
+++ SDET-Project/src/main/java/com/unity3d/project/demo/SdetProjectApplication.java
@@ -3,7 +3,7 @@ package com.unity3d.project.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.unity3d.project" })
+@SpringBootApplication(scanBasePackages = { "com.unity3d.project" }, exclude = org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class)
 public class SdetProjectApplication {
 
 	public static void main(String[] args) {
