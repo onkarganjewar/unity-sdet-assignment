1b07913299ffaef90be524c583d7f3ad74b6a160
Onkar Ganjewar <ganjewaronkar@gmail.com>
2017-05-21 22:45:57 -0700

    Remove unnecessary files

--- SDET-Project/src/main/java/com/unity3d/project/demo/SdetProjectApplication.java
+++ SDET-Project/src/main/java/com/unity3d/project/demo/SdetProjectApplication.java
@@ -1,12 +1,23 @@
 package com.unity3d.project.demo;
 
 import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.builder.SpringApplicationBuilder; 
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.unity3d.project" }, exclude = org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class)
-public class SdetProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SdetProjectApplication.class, args);
-	}
+@SpringBootApplication(scanBasePackages = { "com.unity3d.project" }, exclude = org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class)
+public class SdetProjectApplication extends SpringBootServletInitializer {
+
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(SdetProjectApplication.class);
+	    }
+
+	    public static void main(String[] args) throws Exception {
+	        SpringApplication.run(SdetProjectApplication.class, args);
+	    }
+	    
+	    
 }
